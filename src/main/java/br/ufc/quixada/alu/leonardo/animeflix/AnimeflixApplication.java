84293b80bd1f235956c7176b171559d8c31a9fee
Leonardo <leonardo123k@gmail.com>
2021-11-23 02:42:55 -0300

    setup: initial commit

---
+++ src/main/java/br/ufc/quixada/alu/leonardo/animeflix/AnimeflixApplication.java
@@ -0,0 +1,12 @@
+package br.ufc.quixada.alu.leonardo.animeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeflixApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufc/quixada/alu/leonardo/animeflix/AnimeflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufc.quixada.alu.leonardo.animeflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
